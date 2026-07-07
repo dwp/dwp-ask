a04ee2ab155c22f69f67b3671c94e4c594c88627
Ranj Sidhu <ranjeet.sidhu@engineering.digital.dwp.gov.uk>
2026-07-07 10:43:24 +0100

    fix: remove chat details page
    GitOrigin-RevId: 8d03aae41928079b9b5e250405f2b673c675a898

--- app/(pages)/chat/view-details/page.tsx
+++
@@ -1,8 +0,0 @@
-import { ChatMetadata } from "@/constants/PageMetadata";
-import ChatView from "./ChatView";
-
-export const metadata = ChatMetadata;
-
-export default function ChatViewPage() {
-  return <ChatView />;
-}
